7bec369cfd0f2d5b18c0be5d15d0176b928bf2a0
xzsanz <luissanx99@gmail.com>
2025-07-08 00:28:49 -0700

    Modelos para mongoDb de user y product, en models/user, models/product

---
+++ ejercicios/CRUD/models/Product.js
@@ -0,0 +1,8 @@
+const mongoose= require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    name : String,
+    price: Number,
+    description : String,
+});
+module.exports = mongoose.model("Product",productSchema);
